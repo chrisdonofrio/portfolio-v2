d5206391aa5a1e76bf71f23451d97c109ac6fcdf
Chris Donofrio <cdonofrio20@gmail.com>
2016-04-05 13:43:57 -0400

    fixing PORT bug

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
