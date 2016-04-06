97ba1765c97c23d0ec710881025488cab97824e4
Chris Donofrio <cdonofrio20@gmail.com>
2016-04-06 16:46:22 -0400

    initial setup of nodemailer and contact form

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var app = express();
+var nodemailer = require("nodemailer");
 var PORT = process.env.PORT || 3000;
+
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+  // auth here
+});
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + "/index.html");
+});
+
+app.post('/send',function(req, res) {
+    var mailOptions={
+        from : req.query.from,
+        to : 'cdonofrio20@gmail.com',
+        subject : req.query.subject,
+        text : req.query.text
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
 });
 
 app.listen(PORT, function() {
